5b1c714494326672ec42d5da4f237756012c39ef
nemanjaDobranic <dobranicnemanja2014@gmail.com>
2022-06-03 21:46:10 +0200

    Endponit permits only GET method

--- PhoneNumbers/src/main/API.java
+++ PhoneNumbers/src/main/API.java
@@ -6,25 +6,29 @@ import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+
 public class API {
-    public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/api/v1/phone-numbers/autocomplete", new MyHandler());
-        //Thread control is given to executor service.
-        server.setExecutor(java.util.concurrent.Executors.newCachedThreadPool());
-        server.start();
-    }
-    static class MyHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange t) throws IOException {
-            String response = "There will be numbers :)";
-            long threadId = Thread.currentThread().getId();
-            System.out.println("I am thread " + threadId );
-            response = response + "Thread Id = "+threadId;
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+		server.createContext("/api/v1/phone-numbers/autocomplete", new PhoneHttpHandler());
+		// Thread control is given to executor service.
+		server.setExecutor(java.util.concurrent.Executors.newCachedThreadPool());
+		server.start();
+		System.out.println("Server is up and running on port 8000");
+	}
+
+	static class PhoneHttpHandler implements HttpHandler {
+		@Override
+		public void handle(HttpExchange exchange) throws IOException {
+			if ("GET".equals(exchange.getRequestMethod())) {
+				String response = "There will be numbers :)";
+				exchange.sendResponseHeaders(200, response.length());
+				OutputStream output = exchange.getResponseBody();
+				output.write(response.getBytes());
+				output.close();
+			} else {
+				exchange.sendResponseHeaders(405, -1);// 405 Method Not Allowed
+			}
+		}
+	}
 }
